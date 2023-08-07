4cf39eb6e45280f33b9ab086cef7dc91d1826efe
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2023-08-07 20:06:17 +0700

    refactor

--- python/md5_dir_celery_visualization.py
+++ python/md5_dir_celery_visualization.py
@@ -1,9 +1,9 @@
 import os
 import sys
-from typing import Dict
+from typing import Dict, List
 
 from PySide6 import QtGui
-from PySide6.QtCore import QPoint
+from PySide6.QtCore import QPoint, QRect
 from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QWidget, QApplication
 
@@ -28,65 +28,78 @@ class FileProcessingWidget(QWidget):
     def __init__(self, dir_name: str):
         super(FileProcessingWidget, self).__init__()
 
+        # cell_size = 12  # 16
+        # cell_width = 12
+        self._cell_height = 12
+        self._margin = 4  # 6
+        self._indent = int(self._margin / 2)
+        # internal_size = cell_size - margin
+        # internal_width = cell_width - margin
+        self._internal_height = self._cell_height - self._margin
+
         self._dir_name = dir_name
         self._file_infos = get_file_infos(self._dir_name)
         self._total_bytes = sum(fi for _, fi in self._file_infos.items())
 
-        self.setGeometry(300, 300, 280, 170)
+        self._spans = None
+
+        self.setGeometry(0, 0, 1280, 720)
         self.setWindowTitle('MD5 Calculation')
         self.show()
 
     def paintEvent(self, event):
-
         rect = event.rect()
         print(rect)
 
         qp = QtGui.QPainter()
         qp.begin(self)
 
-        cell_size = 12  # 16
-        margin = 4  # 6
-        indent = int(margin / 2)
-        internal_size = cell_size - margin
-
-        cols = rect.width() // cell_size
-        rows = rect.height() // cell_size
-
-        # calculate number of bytes per 1pix
-        row_len_pix = rect.width() - indent * 2
-        total_strip_len = rows * row_len_pix
-        bytes_per_pix = self._total_bytes / total_strip_len
-
-        print(f'{rows}x{cols}   {total_strip_len}, {self._total_bytes} bytes === {bytes_per_pix:.1f} bpp, pix_per_line={row_len_pix}')
-
         qp.setPen(QColor(0xb9, 0xdb, 0x92))
         # qp.setBrush(QBrush(QColor(0xb9, 0xdb, 0x92)))
 
+        for span in self._spans:
+            qp.drawRect(span)
+
+        qp.end()
+
+    def resizeEvent(self, event):
+        self._spans = self._calc_spans(QRect(QPoint(0, 0), event.size()))
+
+    def _calc_spans(self, window_rect: QRect) -> List[QRect]:
+        out = list()
+
+        rows = window_rect.height() // self._cell_height
+
+        # calculate number of bytes per 1pix
+        row_len_pix = window_rect.width() - self._indent * 2
+        total_strip_len = rows * row_len_pix
+        bytes_per_pix = self._total_bytes / total_strip_len
+
         file_num = 0
         cur_line_num = 0
-        next_start_pt = rect.topLeft() + QPoint(indent, indent)
+        next_start_pt = window_rect.topLeft() + QPoint(self._indent, self._indent)
         for filename, file_size in self._file_infos.items():
             file_len_pix = file_size / bytes_per_pix
 
             print(f'#{file_num}: name={filename} size={file_size} len_pix={file_len_pix}')
 
             pix_left = file_len_pix
-            cur_line_len_pix = rect.width() - next_start_pt.x() - indent
+            cur_line_len_pix = window_rect.width() - next_start_pt.x() - self._indent
             while pix_left >= cur_line_len_pix:
-                qp.drawRect(next_start_pt.x(), next_start_pt.y(), cur_line_len_pix, internal_size)
-                next_start_pt.setX(indent)
-                next_start_pt.setY(next_start_pt.y() + cell_size)
+                out.append(QRect(next_start_pt.x(), next_start_pt.y(), cur_line_len_pix, self._internal_height))
+                next_start_pt.setX(self._indent)
+                next_start_pt.setY(next_start_pt.y() + self._cell_height)
                 pix_left -= cur_line_len_pix
-                cur_line_len_pix = rect.width() - next_start_pt.x() - indent
+                cur_line_len_pix = window_rect.width() - next_start_pt.x() - self._indent
                 cur_line_num += 1
                 print(f'next line ({cur_line_num})')
 
-            qp.drawRect(next_start_pt.x(), next_start_pt.y(), pix_left, internal_size)
-            next_start_pt.setX(next_start_pt.x() + pix_left + indent * 2)
+            out.append(QRect(next_start_pt.x(), next_start_pt.y(), pix_left, self._internal_height))
+            next_start_pt.setX(next_start_pt.x() + pix_left + self._indent * 2)
 
             file_num += 1
 
-        qp.end()
+        return out
 
 
 def main():
