3021fff0f88c6d4fc813a9a8e51a04af5b3ba622
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2023-08-07 19:51:42 +0700

    [wip]

---
+++ python/md5_dir_celery_visualization.py
@@ -0,0 +1,102 @@
+import os
+import sys
+from typing import Dict
+
+from PySide6 import QtGui
+from PySide6.QtCore import QPoint
+from PySide6.QtGui import QColor
+from PySide6.QtWidgets import QWidget, QApplication
+
+
+def get_file_infos(dir_name: str) -> Dict[str, int]:
+    file_infos = dict()
+
+    for file in os.listdir(dir_name):
+        if file.endswith('.sqlite'):
+            continue
+
+        full_filename = os.path.join(dir_name, file)
+        if not os.path.isfile(full_filename):
+            continue
+
+        file_infos[full_filename] = os.stat(full_filename).st_size
+
+    return file_infos
+
+
+class FileProcessingWidget(QWidget):
+    def __init__(self, dir_name: str):
+        super(FileProcessingWidget, self).__init__()
+
+        self._dir_name = dir_name
+        self._file_infos = get_file_infos(self._dir_name)
+        self._total_bytes = sum(fi for _, fi in self._file_infos.items())
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('MD5 Calculation')
+        self.show()
+
+    def paintEvent(self, event):
+
+        rect = event.rect()
+        print(rect)
+
+        qp = QtGui.QPainter()
+        qp.begin(self)
+
+        cell_size = 12  # 16
+        margin = 4  # 6
+        indent = int(margin / 2)
+        internal_size = cell_size - margin
+
+        cols = rect.width() // cell_size
+        rows = rect.height() // cell_size
+
+        # calculate number of bytes per 1pix
+        row_len_pix = rect.width() - indent * 2
+        total_strip_len = rows * row_len_pix
+        bytes_per_pix = self._total_bytes / total_strip_len
+
+        print(f'{rows}x{cols}   {total_strip_len}, {self._total_bytes} bytes === {bytes_per_pix:.1f} bpp, pix_per_line={row_len_pix}')
+
+        qp.setPen(QColor(0xb9, 0xdb, 0x92))
+        # qp.setBrush(QBrush(QColor(0xb9, 0xdb, 0x92)))
+
+        file_num = 0
+        cur_line_num = 0
+        next_start_pt = rect.topLeft() + QPoint(indent, indent)
+        for filename, file_size in self._file_infos.items():
+            file_len_pix = file_size / bytes_per_pix
+
+            print(f'#{file_num}: name={filename} size={file_size} len_pix={file_len_pix}')
+
+            pix_left = file_len_pix
+            cur_line_len_pix = rect.width() - next_start_pt.x() - indent
+            while pix_left >= cur_line_len_pix:
+                qp.drawRect(next_start_pt.x(), next_start_pt.y(), cur_line_len_pix, internal_size)
+                next_start_pt.setX(indent)
+                next_start_pt.setY(next_start_pt.y() + cell_size)
+                pix_left -= cur_line_len_pix
+                cur_line_len_pix = rect.width() - next_start_pt.x() - indent
+                cur_line_num += 1
+                print(f'next line ({cur_line_num})')
+
+            qp.drawRect(next_start_pt.x(), next_start_pt.y(), pix_left, internal_size)
+            next_start_pt.setX(next_start_pt.x() + pix_left + indent * 2)
+
+            file_num += 1
+
+        qp.end()
+
+
+def main():
+    app = QApplication(sys.argv)
+    DIR = '/Users/dipaolo/allure-report/history'
+    DIR = '.'
+    DIR = '/Users/dipaolo/Downloads'
+    ex = FileProcessingWidget(DIR)
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
